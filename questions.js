55a97ffc71cb3ec4355ebbb53bac73adc9546358
George Benainous <georgesb@mac.com>
2023-12-19 20:50:20 -0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,33 @@
+const myQuestions =
+[
+  {
+    "question": "Chat",
+    "answers": {
+      "a": "Dog",
+      "b": "Cat",
+      "c": "Bird",
+      "d": "Fish"
+    },
+    "correctAnswer": "b"
+  },
+  {
+    "question": "Livre",
+    "answers": {
+      "a": "Book",
+      "b": "Table",
+      "c": "Chair",
+      "d": "Pen"
+    },
+    "correctAnswer": "a"
+  },
+  {
+    "question": "Au revoir",
+    "answers": {
+      "a": "Hello",
+      "b": "Goodbye",
+      "c": "Thank you",
+      "d": "Sorry"
+    },
+    "correctAnswer": "b"
+  }
+]
